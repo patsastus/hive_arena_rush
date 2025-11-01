9a6e024af82b9564fff1a26e6b853d9a6bda1dbc
nraatika <nraatika@student.hive.fi>
2025-11-01 10:33:24 +0200

    targeted exploration working implementation

--- nraatika-agent/main.c
+++ nraatika-agent/main.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include "agent.h"
 #include <stdbool.h>
+#include "move.h"
 
 // -----------------------------------------------------------------------------
 
@@ -83,107 +84,6 @@ inline command_t	move_in_random_unblocked_direction(agent_info_t info)
 		.action = MOVE,
 		.direction = dir
 	};
-}
-
-static command_t targeted_explore(agent_info_t info)
-{
-	static int	switch_after = 100;
-	coords_t targets[NUM_BEES][2] = {
-		{{3, 3}, {3, 10}},
-		{{3, 16}, {3, 22}},
-		{{9, 16}, {16, 16}},
-		{{22, 16}, {22, 22}},
-		{{22, 3}, {22, 10}}
-	};
-	
-	return (try_going(info, go_to_coords(info, targets[info.bee][(info.turn / switch_after) % 2])));
-}
-
-static dir_t go_to_coords(agent_info_t info, coords_t target)
-{
-	int	dx = target.col - info.col;
-	int dy = target.row - info.row;
-
-	if (dx == 0 || dy == 0)
-	{
-		if (dx == 0)
-			return (dy < 0 ? N : S);
-		else
-			return (dx < 0 ? W : E);
-	}
-	if (abs(dx) * 2 < abs(dy))
-		return (dy < 0 ? N : S);
-	else if (abs(dy) * 2 < abs(dx))
-		return (dx < 0 ? W : E);
-	else
-	{
-		if (dx < 0)
-			return (dy < 0 ? NW : SW);
-		else
-			return (dy < 0 ? NE : SE);
-	}
-}	
-
-//go toward home
-static dir_t go_home(agent_info_t info)
-{
-	dir_t goal = W - 4*info.player;
-	int home[2] = {12, 1};
-	if (info.player)
-	{
-		home[1] = 28;
-		if (info.row - home[0] > 0)
-			goal = NE;
-		else if (info.row - home[0] < 0)
-			goal = SE;
-		if (info.col >= 27)
-			goal = N + (info.row - home[0] < 0)*4;
-	}
-	else
-	{
-		if (info.row - home[0] > 0)
-			goal = NW;
-		else if (info.row - home[0] < 0)
-			goal = SW;
-		if (info.col <= 2)
-			goal = N + (info.row - home[0] < 0)*4;
-	}
-	return goal;
-}
-
-static cell_t	get_cell_type(agent_info_t info, dir_t d)
-{
-	int	x = 0;
-	int y = 0;
-
-	if (d == N || d == NW || d == NE)
-		y = -1;
-	if (d == S || d == SW || d == SE)
-		y = 1;
-	if (d == E || d == NE || d == SE)
-		x = 1;
-	if (d == W || d == NW || d == SW)
-		x = -1;
-	return (info.cells[VIEW_DISTANCE+y][VIEW_DISTANCE+x]);
-}
-
-static command_t try_going(agent_info_t info, dir_t direction)
-{
-	printf("Bee at [%d,%d] going towards %d\n", info.row, info.col, direction);
-	if (get_cell_type(info, direction) == EMPTY)
-	{
-		return (command_t) {
-                .action = MOVE,
-                .direction = direction
-            };
-	}
-	else 
-	{
-		return (command_t) {
-        	.action = MOVE,
-	        .direction = rand() % 8
-    	};
-	}
 }
 
 int find_neighbour(agent_info_t info, cell_t type)
